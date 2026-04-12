ab3c211c1a4ee494e059c94b88c26addbc73096f
Wulius <werbesan@gmail.com>
2026-04-12 12:11:52 +0200

    Extend hotel settings with design system fields

---
+++ prisma/migrations/20260412101120_extend_hotel_settings_design/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "HotelSettings" ADD COLUMN     "borderColor" TEXT,
+ADD COLUMN     "buttonRadius" INTEGER,
+ADD COLUMN     "cardBackground" TEXT,
+ADD COLUMN     "cardRadius" INTEGER,
+ADD COLUMN     "mutedTextColor" TEXT,
+ADD COLUMN     "textColor" TEXT;
